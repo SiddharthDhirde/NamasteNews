704dea1719e9fa24fedfc33b4db4bead184b7612
SiddharthDhirde <SiddharthDhirde09@gmail.com>
2022-10-15 22:47:28 +0530

    Ready for Deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+// /api/new-meetup
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://Shivaji:Shivaji@cluster0.pfohnuv.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
